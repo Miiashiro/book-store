0f6e092f81e06d0641186938a43c992426b795f9
Miiashiro <miiashiro.caio1@gmail.com>
2024-10-14 16:51:05 -0300

    Função de remover livros do carrinho

--- js/books.js
+++ js/books.js
@@ -4,17 +4,18 @@ var modal = document.querySelector('.modal');
 var itemCart = document.querySelector('.itens');
 var showSpan = document.querySelector('#show');
 var total = document.querySelector('.total');
-var badge = document.querySelector('#badge')
+var badge = document.querySelector('#badge');
 
 var sumPrice = 0;
 
+// Fechamento do modal
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.close();
     };
 });
 
-//itens
+// Itens
 const books = [
     {
         id: 1,
@@ -54,9 +55,10 @@ const books = [
     }
 ];
 
-const cartShopping = []
+// Array para os itens dentro do carrinho
+const cartShopping = [];
 
-//funcao de mostrar os livros
+// Funcao de mostrar os livros
 const gridBooks = (books) => {
     books.map((books) => {
         //cartão do livro
@@ -64,31 +66,32 @@ const gridBooks = (books) => {
         div.classList.add("card");
         container.appendChild(div);
 
-        //imagem do livro
+        //Imagem do livro
         const imagem = document.createElement("img");
         imagem.src = books.img;
-        imagem.classList.add("books-image")
+        imagem.classList.add("books-image");
         div.appendChild(imagem);
 
-        //nome do livro
+        //Nome do livro
         const title = document.createElement("p");
         title.classList.add("nome");
         title.innerHTML = books.bookName;
         div.appendChild(title);
 
-        //preco do livro
+        //Preco do livro
         const value = document.createElement("p");
         value.classList.add("price");
         value.innerHTML = `R$${books.price}`;
         div.appendChild(value);
 
-        //botao de adicionar
+        //Botao de adicionar
         const buttonAdd = document.createElement("button");
         buttonAdd.classList.add("add");
         buttonAdd.innerHTML = "Adicionar";
+        buttonAdd.setAttribute("attr", books.id);
         div.appendChild(buttonAdd);
 
-        //adiciona no array o livro que o usuario clicou
+        // Adicionar ao carrinho o livro que o usuario clicou
         buttonAdd.addEventListener("click", function(){
             buttonAdd.setAttribute("disabled", "true");  // Desabilita o botão após o clique, para não dublicar valores no array cartShopping
 
@@ -105,81 +108,82 @@ const gridBooks = (books) => {
             showItemCart(buttonAdd);
 
             badge.className = badge.className.replace('hide', 'show');
-        })
+        });
     });
 };
 
-//Abrir o modal do carrinho de vendas
+// Abrir o modal do carrinho de vendas
 dialog.addEventListener("click", function(){
     modal.showModal();
 
-    //esconde a frase de nao haver livros adicionados
+    //Esconde a frase de nao haver livros adicionados
     if (sumPrice > 0) {
         if(showSpan.className == "show"){
-            showSpan.classList.remove("show")
-        }
+            showSpan.classList.remove("show");
+        };
+
         showSpan.classList.add("hide");
     }
 });
 
-//Mostrar livros no carrinho
-function showItemCart(btnAdd) {
+// Mostrar livros no carrinho
+function showItemCart() {
 
     itemCart.innerHTML = "";
 
     cartShopping.map((booksCart, index) => {
-        //div
+        //Div
         const divCart = document.createElement("div");
         divCart.classList.add("item");
         itemCart.appendChild(divCart);
 
-        //Quando o dialog atualiza as divs perdem a class hide line 223
+        //Quando o dialog atualiza as divs perdem a class hide
         if (booksCart.visible === false) {
             divCart.classList.toggle("hide"); //Esconde os elementos para que não haja repetição do mesmo apos a deletacao
         };
 
-        //imagem do livro
+        //Imagem do livro
         const imgItemCart = document.createElement("img");
-        imgItemCart.classList.add("img_item_cart")
+        imgItemCart.classList.add("img_item_cart");
         imgItemCart.src = booksCart.img;
         divCart.appendChild(imgItemCart);
 
-        //div envolta do nome do livro
+        //Div envolta do nome do livro
         const wrapNameCart = document.createElement("div");
         wrapNameCart.classList.add("wrap_name_cart");
         divCart.appendChild(wrapNameCart);
 
-        //nome do livro
+        //Nome do livro
         const nameBookCart = document.createElement("span");
         nameBookCart.classList.add("name_book_cart");
         nameBookCart.innerHTML = booksCart.bookName;
         wrapNameCart.appendChild(nameBookCart);
 
-        //preco
+        //Preco
         const priceCart = document.createElement("span");
         priceCart.classList.add("price_cart");
         priceCart.innerHTML = `R$ ${booksCart.price}`;
         divCart.appendChild(priceCart);
 
-        //div envolta da quantidade
+        //Div envolta da quantidade
         const wrapQuantCart = document.createElement("div");
-        wrapQuantCart.classList.add("wrap_quant_cart")
+        wrapQuantCart.classList.add("wrap_quant_cart");
         divCart.appendChild(wrapQuantCart);
 
-        //botao de retirar
+        //Botao de retirar
         const buttonMinus = document.createElement("button");
         buttonMinus.classList.add("button_minus");
         buttonMinus.innerHTML = "-";
         wrapQuantCart.appendChild(buttonMinus);
 
-        //input marcando a quantidade comprada
+        //Input marcando a quantidade comprada
         const quantityCart = document.createElement("input");
         quantityCart.classList.add("quantity_cart");
         quantityCart.value = booksCart.quantity;
         quantityCart.disabled = true;
         wrapQuantCart.appendChild(quantityCart);
 
-        //botao de adicionar
+        //Botao de adicionar
         const buttonPlus = document.createElement("button");
         buttonPlus.classList.add("button_plus");
         buttonPlus.innerHTML = "+";
@@ -191,10 +195,10 @@ function showItemCart(btnAdd) {
         //Remocao do livro
         const removeItem = document.createElement("span");
         removeItem.classList.add("remove_item");
-        removeItem.innerHTML = "X"
+        removeItem.innerHTML = "X";
         divCart.appendChild(removeItem);
 
-        //diminuir a quantidade de livros do input
+        //Diminuir a quantidade de livros do input
         buttonMinus.addEventListener("click", function(){
             if (quantityCart.value > 1) {
                 booksCart.quantity--;
@@ -204,7 +208,7 @@ function showItemCart(btnAdd) {
             }
         })
 
-        //aumentar a quantidade de livros do input
+        //Aumentar a quantidade de livros do input
         buttonPlus.addEventListener("click", function(){
             booksCart.quantity++;
             quantityCart.value = booksCart.quantity;
@@ -212,29 +216,29 @@ function showItemCart(btnAdd) {
             total.innerHTML = `Total: R$ ${sumPrice += parseInt(booksCart.price)}`;
         })
 
-        //remover livro no carrinho
+        //Remover livro no carrinho
         removeItem.addEventListener("click", function(){
-            total.innerHTML = `Total: R$ ${sumPrice -= (booksCart.quantity * parseInt(booksCart.price))}`
+            total.innerHTML = `Total: R$ ${sumPrice -= (booksCart.quantity * parseInt(booksCart.price))}`;
 
-            cartShopping[index].visible = false; //adiciona uma propriedade para esconder o elemento
-
+            cartShopping[index].visible = false; //Adiciona uma propriedade para esconder o elemento
             
-            //btnAdd.disabled = !btnAdd.disabled; //ativa o botao para adicionar o mesmo livro ao carrinho
-            
+            // Verifica se o carrinho está vazio para atualizar a badge e mensagem
             if(sumPrice == 0){
                 badge.className = badge.className.replace('show', 'hide');
                 showSpan.className = showSpan.className.replace('hide', 'show');
-            }
+            };
 
-            
-            if(btnAdd.disabled === true){
-//                btnAdd.removeAttribute("disabled"); //ativa o botao para adicionar o mesmo livro ao carrinho
-//                btnAdd.disabled = false;
-                console.log(btnAdd)
-            }
+            // Reativar o botão de "Adicionar" correspondente ao livro removido 
+            let button = document.querySelector(`button[attr="${booksCart.id}"]`);
+
+            if (button) {
+                button.disabled = false;
+            };
+
+            // Esconde o item removido no carrinho
             divCart.classList.add("hide");
-        })
+        });
     });
-}
+};
 
-gridBooks(books)
+gridBooks(books);
